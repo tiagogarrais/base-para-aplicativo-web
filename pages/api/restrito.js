f9d18ea1ebce30ee2d9278255619cf283f7ed9d9
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2023-09-03 15:01:26 +0000

    Try fix bug

--- pages/api/restrito.js
+++ pages/api/restrito.js
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next"
-import { authOptions } from "/pages/api/auth/[...nextauth]"
+import { authOptions } from "/api/auth/[...nextauth].js"
 
 export default async (req, res) => {
   const session = await getServerSession(req, res, authOptions)
