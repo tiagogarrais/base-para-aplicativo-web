10f4ef22c3b1916e60974d3a58dca170016a677a
Tiago das Graças Arrais <tiagoarrais1981@hotmail.com>
2024-08-03 00:22:58 -0300

    Update dependencies
    Atualizando dependências para versões mais atualizadas e seguras

--- pages/acesso-com-login.js
+++ pages/acesso-com-login.js
@@ -1,23 +1,27 @@
-import { useSession, signIn, signOut } from "next-auth/react"
-import Link from 'next/link'
+import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
 
 export default function AcessoComLogin() {
-  const { data: session } = useSession()
+  const { data: session } = useSession();
   if (session) {
     return (
       <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-        <p><Link href='/'>Voltar para a página inicial</Link></p>
+        Você está autenticado com o seguinte e-mail: {session.user.email} <br />
+        <button onClick={() => signOut()}>Desconectar</button>
+        <p>
+          <Link href="/">Voltar para a página inicial</Link>
+        </p>
       </>
-    )
+    );
   }
   return (
     <>
-      Você não está logado <br />
-      <button onClick={() => signIn()}>Entrar</button><br/>
-      <p><Link href='/'>Voltar para a página inicial</Link></p>
-
+      Você não está autenticado <br />
+      <button onClick={() => signIn()}>Entrar</button>
+      <br />
+      <p>
+        <Link href="/">Voltar para a página inicial</Link>
+      </p>
     </>
-  )
+  );
 }
